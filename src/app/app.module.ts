09ff3a950f2bd28bb365bfc6dab23cc0d960bfa0
arunselvakumar <b.arunselvakumar@gmail.com>
2018-10-20 10:47:57 +0000

    Adsense Merged

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AngularFontAwesomeModule } from 'angular-font-awesome';
+import { AdsenseModule } from 'ng2-adsense';
+import { AppRoutingModule } from './modules/app-routing/app-routing.module';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/root/header/header.component';
 import { HomeComponent } from './components/home/home/home.component';
 import { NavbarComponent } from './components/home/navbar/navbar.component';
 import { SiteComponent } from './components/home/site/site.component';
-import {AppRoutingModule} from './modules/app-routing/app-routing.module';
 import { BlogComponent } from './components/blog/blog/blog.component';
 import { HelpComponent } from './components/help/help/help.component';
 import { FeedbackComponent } from './components/feedback/feedback/feedback.component';
@@ -31,6 +32,7 @@ import { ImageCardComponent } from './components/shared/image-card/image-card.co
     ImageCardComponent
   ],
   imports: [
+    AdsenseModule.forRoot({ adClient: 'ca-pub-6185517953080782', adSlot: 6187145078 }),
     BrowserModule,
     AngularFontAwesomeModule,
     AppRoutingModule
